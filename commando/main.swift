eac19ad56756f299588d66d86c68f9abd80ddb90
Uli Kusterer <ulivc@zathras.de>
2019-07-12 21:41:13 +0200

    Initial Commit

---
+++ commando/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  commando
+//
+//  Created by Uli Kusterer on 12.07.19.
+//  Copyright © 2019 Uli Kusterer. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
